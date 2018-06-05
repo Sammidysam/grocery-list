59d61053aa6c4c9facc5490b797336c22096a53d
Sam Craig <craigsamm@gmail.com>
2018-06-05 13:39:49 -0400

    Clarify that non-integers are allowed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const changeHeading = function(ev) {
 
   // If the spell level is not a number, turn it into one.
   if (!isNumeric(spellLevel)) {
-	  errorBox.innerHTML = `${spellName} must be given an integer level<br>Default value of ${DEFAULT_LEVEL} used`
+	  errorBox.innerHTML = `${spellName} must be given a numeric level<br>Default value of ${DEFAULT_LEVEL} used`
 	  spellLevel = DEFAULT_LEVEL
   } else {
 	  errorBox.innerHTML = ''
