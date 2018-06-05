b7d8b3f830bf6c924941b07f3f2f74435757f35b
Sam Craig <craigsamm@gmail.com>
2018-06-05 13:30:15 -0400

    Switch to using document.createElement and appendChild to add to the spell list

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const changeHeading = function(ev) {
   const spellName = f.spellName.value
   let spellLevel = f.spellLevel.value
 
+  // If the spell level is not a number, turn it into one.
   if (!isNumeric(spellLevel)) {
 	  errorBox.innerHTML = `${spellName} must be given an integer level<br>Default value of ${DEFAULT_LEVEL} used`
 	  spellLevel = DEFAULT_LEVEL
@@ -24,7 +25,11 @@ const changeHeading = function(ev) {
   }
 
   const spellsDiv = document.querySelector('#spells')
-  spellsDiv.innerHTML += `<li>${spellName}, Level ${spellLevel}</li>`
+  
+  const newItem = document.createElement('li')
+  newItem.appendChild(document.createTextNode(`${spellName}, Level ${spellLevel}`))
+
+  spellsDiv.appendChild(newItem)
 
   f.reset()
 }
