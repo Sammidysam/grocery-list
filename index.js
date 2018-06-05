d95a4f0fb6bfde1ea70bef2cbc911a6d1e8822ce
Sam Craig <craigsamm@gmail.com>
2018-06-05 13:09:40 -0400

    Overwrite invalid spell level

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const form = document.querySelector('form')
 
+// This checks if a string is a number or not.
+// Borrowed from https://stackoverflow.com/questions/9716468/is-there-any-function-like-isnumeric-in-javascript-to-validate-numbers
+const isNumeric = function(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
 const changeHeading = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
   const spellName = f.spellName.value
-  const spellLevel = f.spellLevel.value
+  let spellLevel = f.spellLevel.value
+
+  if (!isNumeric(spellLevel)) {
+	  spellLevel = 1
+  }
 
   const spellsDiv = document.querySelector('#spells')
   spellsDiv.innerHTML += `<li>${spellName}, Level ${spellLevel}</li>`
